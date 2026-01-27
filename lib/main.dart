df76d2039a9fc21011355d3a609df501115ff49e
Desalegn <yetdese18@gmail.com>
2026-01-27 14:20:41 +0300

    Initial commit: offline-first property listing app

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+
+import 'package:flutter/material.dart';
+void main() {
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Container(
+        color: Colors.white,
+        child: Center(
+          child: Text(
+            'My Property List App!',
+            style: TextStyle(fontSize: 24, color: Colors.black),
+          ),
+        ),
+      ),
+    );
+  }
+}
